0c1cbd5243a0f09f62b756c9a82b348ab4132a6e
Trino Calma <tjcalma@gmail.com>
2024-10-17 23:28:21 -0300

    fix: installer file config

--- src/Installer.php
+++ src/Installer.php
@@ -9,17 +9,12 @@ class Installer
         $currentWorkingDir = getcwd();
         $configDir = realpath($currentWorkingDir . '/config/packages');
 
-        if ($configDir === false) {
-            // Si el directorio no existe, crearlo
-            $configDir = $currentWorkingDir . '/config/packages';
-            mkdir($configDir, 0755, true);
-        }
 
-        $configFile = $configDir . '/form_schema_builder.yaml';
+        if ($configDir !== false) {
+            $configFile = $configDir . '/form_schema_builder.yaml';
 
-        if (!file_exists($configFile)) {
-            if (!copy(__DIR__ . '/../Resources/config/form_schema_builder.yaml', $configFile)) {
-                throw new \RuntimeException('Error al copiar el archivo de configuración.');
+            if (!file_exists($configFile)) {
+                copy(__DIR__ . '/../Resources/config/form_schema_builder.yaml', $configFile);
             }
         }
     }
